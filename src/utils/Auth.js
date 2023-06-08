e57fcf270fec4bfe136f5f4f182b8e16a6476805
Ivan Vorobyov <ivanvorobyov1992@yandex.ru>
2023-06-08 21:54:33 +0300

    Create Auth.js

---
+++ src/utils/Auth.js
@@ -0,0 +1,53 @@
+class Auth {
+  constructor(baseUrl) {
+    this._baseUrl = baseUrl;
+  }
+
+  _checkResponse(res) {
+    if (res.ok) {
+      return res.json();
+    } else {
+      return Promise.reject(`Ошибка: ${res.status} ${res.statusText}`);
+    }
+  }
+
+  _request(url, options) {
+    return fetch(url, options).then(this._checkResponse);
+  }
+
+  registerUser({ email, password }) {
+    return this._request(`${this._baseUrl}/signup`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({email, password}),
+    }).then(res => this._checkResponse(res));
+  }
+
+  loginUser({ email, password }) {
+    return this._request(`${this._baseUrl}/signin`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({email, password}),
+    }).then(res => this._checkResponse(res));
+  }
+
+  checkToken(token) {
+    return this._request(`${this._baseUrl}/signin`, {
+      headers: {
+        'Content-Type': 'application/json',
+        "Authorization" : `Bearer ${token}`
+      }
+    }).then(res => this._checkResponse(res));
+  }
+}
+
+const auth = new Auth({
+  baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-64',
+
+});
+
+export default auth;
