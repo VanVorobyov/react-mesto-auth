c6ede47a1f9feec10fd3b69f7cacbc5bacf56dc7
Ivan Vorobyov <ivanvorobyov1992@yandex.ru>
2023-06-14 08:00:25 +0300

    feat: add popup component

--- src/components/InfoTooltip.jsx
+++ src/components/InfoTooltip.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
-import usePopupClose from '../hooks/usePopupClose';
+import Popup from './Popup';
 
 const InfoTooltip = (props) => {
   const { isSuccess, isOpen, onClose } = props;
-  usePopupClose(isOpen, onClose);
   return (
-    <div className={`popup ${isOpen ? 'popup_opened' : ''}`}>
-      <div className="popup__container">
-        <button
-          type="button"
-          className={`popup__close-button`}
-          onClick={onClose}
-        ></button>
-        <div className={`${isSuccess ? 'popup__icon-tooltip-sucсess' : 'popup__icon-tooltip-error'}`}></div>
-        <h2 className="popup__title popup__title_center">
-          {isSuccess ? 'Вы успешно зарегистрировались!' : 'Что-то пошло не так! Попробуйте ещё раз.'}
-        </h2>
-      </div>
-    </div>
+    <Popup
+      isOpen={isOpen}
+      onClose={onClose}
+    >
+      <button
+        type="button"
+        className={`popup__close-button`}
+        onClick={onClose}
+      ></button>
+      <div className={`${isSuccess ? 'popup__icon-tooltip-sucсess' : 'popup__icon-tooltip-error'}`}></div>
+      <h2 className="popup__title popup__title_center">
+        {isSuccess ? 'Вы успешно зарегистрировались!' : 'Что-то пошло не так! Попробуйте ещё раз.'}
+      </h2>
+    </Popup>
   );
 };
 
--- src/components/PopupWithForm.jsx
+++ src/components/PopupWithForm.jsx
@@ -1,36 +1,38 @@
-import usePopupClose from '../hooks/usePopupClose';
+import Popup from './Popup';
 
 function PopupWithForm(props) {
   const { title, name, children, isOpen, onClose, onSubmit, buttonText, isValid, isLoading, isDisabled, loadingText } = props;
-  usePopupClose(isOpen, onClose);
+
   return (
-    <div className={`popup popup_type_${name} ${isOpen ? 'popup_opened' : ''}`}>
-      <div className="popup__container">
+    <Popup
+      isOpen={isOpen}
+      name={name}
+      onClose={onClose}
+    >
+      <button
+        type="button"
+        className={`popup__close-button popup__close-button_${name}`}
+        onClick={onClose}
+      />
+      <h2 className="popup__title">{title}</h2>
+      <form
+        className={`popup__form popup__form_${name}`}
+        action="#"
+        method="post"
+        name={`${name}-form`}
+        noValidate
+        onSubmit={onSubmit}
+      >
+        {children}
         <button
-          type="button"
-          className={`popup__close-button popup__close-button_${name}`}
-          onClick={onClose}
-        />
-        <h2 className="popup__title">{title}</h2>
-        <form
-          className={`popup__form popup__form_${name}`}
-          action="#"
-          method="post"
-          name={`${name}-form`}
-          noValidate
-          onSubmit={onSubmit}
+          className={`popup__button popup__button_${name} ${!isValid || isDisabled ? 'popup__button_disabled' : ''}`}
+          type="submit"
+          disabled={!isValid || isDisabled}
         >
-          {children}
-          <button
-            className={`popup__button popup__button_${name} ${!isValid || isDisabled ? 'popup__button_disabled' : ''}`}
-            type="submit"
-            disabled={!isValid || isDisabled}
-          >
-            {isLoading ? loadingText : buttonText}
-          </button>
-        </form>
-      </div>
-    </div>
+          {isLoading ? loadingText : buttonText}
+        </button>
+      </form>
+    </Popup>
   );
 }
 
